544f0325158a0648e8f518dbef1cc9ef71e61e7b
SangJun Han <grw05177@naver.com>
2023-04-11 20:23:38 +0900

    modify explanation

--- BOJ1620.py
+++ BOJ1620.py
@@ -1,18 +1,23 @@
 import sys
 
 N, M = map(int, sys.stdin.readline().split())
+# key 값이 포켓몬 이름으로 들어올 딕셔너리와 key 값이 번호를 들어올 딕셔너리 두개를 만든다.
 Dict = {}
 Dictnum = {}
 
+# 1번 부터 N번까지 돌며 각 딕셔너리에 넣어준다.
 for i in range(1,N+1):
     poketmon = sys.stdin.readline().rstrip()
     Dict[poketmon] = i
     Dictnum[i] = poketmon
 
+# 문제를 입력하고
 for j in range(M):
     problem = sys.stdin.readline().rstrip()
+    # 문제가 int로 바꿀 수 있으면 바꾸고 번호 딕셔너리에서
     try:
         problem = int(problem)
         print(Dictnum[problem])
+    # 못 바꾸면 포켓폰 이름 딕셔너리에서 찾아 출력해준다.
     except ValueError:
         print(Dict[problem])
--- BOJ7785.py
+++ BOJ7785.py
@@ -1,8 +1,10 @@
 import sys
 
 n = int(sys.stdin.readline())
+# 집합 구조를 이용
 liveCompany = set()
 
+# 변수를 나눠 받아 liveCompany에 넣는다.
 for i in range(n):
     name, do = sys.stdin.readline().split()
     if do == 'enter':
@@ -10,5 +12,6 @@ for i in range(n):
     else:
         liveCompany.remove(name)
 
+# 역으로 정렬하고 출혁한다.
 currentCompany = sorted(liveCompany, reverse=True)
 print('\n'.join(currentCompany))
