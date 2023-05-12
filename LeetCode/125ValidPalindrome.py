21eca2b6a6d72b92ce6c3966309e04d1d69126f1
SangJun <grw05177@naver.com>
2023-05-13 01:57:40 +0900

    #15 LeetCode 125. Valid Palindrome solving

--- LeetCode/125ValidPalindrome.py
+++ LeetCode/125ValidPalindrome.py
@@ -2,5 +2,5 @@ class Solution:
     def isPalindrome(self, s: str) -> bool:
         # 컴프리헨션을 통해 문자열의 알파벳과 숫자만 받아서 배열에 넣는다.
         palindrome = [i.lower() for i in s if i.isalnum()]
-        # 뽑아낸 문자열과 거꾸로가 같으면 True 다르면 False를 반환한다.
+        # 뽑아낸 문자열과 거꾸로가 같으면 True 다르면 False를 반환
         return palindrome == palindrome[::-1] 
